19bd8e515127b26533232943a93fe934d159318a
Ayush <ayushempire03@gmail.com>
2023-07-30 01:56:43 +0530

    created carousel and carousel serchbar VDO3

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Cards from "../components/Cards";
+import Carousal from "../components/Carousal";
 
 export default function Header() {
   return (
     <div>
       <div>
         <Navbar />
+      </div>
+      <div>
+        <Carousal />
       </div>
       <div>
         <Cards />
