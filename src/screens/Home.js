35d15f3dc677754e8597dfd430aed2cb9dae7d36
Ayush <ayushempire03@gmail.com>
2023-08-21 23:41:39 +0530

    make serch bar active

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Cards from "../components/Cards";
-import Carousal from "../components/Carousal";
+// import Carousal from "../components/Carousal";
 
 export default function Header() {
+  //css
+  let img_fit = {
+    objectFit: "cover",
+  };
+
+  const [search, setsearch] = useState("");
   const [food_categories, setfood_Categories] = useState([]);
   const [food_items, setfood_items] = useState([]);
 
@@ -33,7 +39,80 @@ export default function Header() {
         <Navbar />
       </div>
       <div>
-        <Carousal />
+        <div>
+          <div
+            id="carouselExampleFade"
+            className="carousel slide carousel-fade"
+            data-bs-ride="carousel"
+            style={img_fit}
+          >
+            <div className="carousel-inner " id="carousel">
+              <div className="carousel-caption" style={{ zIndex: "10" }}>
+                <div className="d-flex justify-content-center">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={search}
+                    onChange={(e) => {
+                      setsearch(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="carousel-item active">
+                <img
+                  src="https://source.unsplash.com/random/300×300/?burger"
+                  className="d-block w-100"
+                  alt="..."
+                  style={({ filter: "brightness(50%)" }, { img_fit })}
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://source.unsplash.com/random/300×300/?pizza"
+                  className="d-block w-100"
+                  alt="..."
+                  style={({ filter: "brightness(50%)" }, { img_fit })}
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://source.unsplash.com/random/300×300/?pastry"
+                  className="d-block w-100"
+                  alt="..."
+                  style={({ filter: "brightness(50%)" }, { img_fit })}
+                />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleFade"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleFade"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
       </div>
       <div className="container">
         {food_categories !== []
@@ -46,7 +125,11 @@ export default function Header() {
                   <hr />
                   {food_items !== [] ? (
                     food_items
-                      .filter((item) => item.CategoryName === data.CategoryName)
+                      .filter(
+                        (item) =>
+                          item.CategoryName === data.CategoryName &&
+                          item.name.toLowerCase().includes(search.toLowerCase())
+                      )
                       .map((filterItems) => {
                         return (
                           <div
