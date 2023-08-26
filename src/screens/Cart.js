8906853c10a83f8d870da1579c5a9d8ac23d6a80
Ayush <ayushempire03@gmail.com>
2023-08-26 20:42:07 +0530

    partiaally created the model for cart VDO 12

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,19 +1,59 @@
 import React from "react";
+import { useCart, useDispatchCart } from "../components/ContextReducer";
 
 export default function Cart() {
+  let data = useCart();
+  let dispatch = useDispatchCart();
+  if (data.length === 0) {
+    return (
+      <div>
+        <div className="m-5 w-100 text-center fs-3">The Cart is empty</div>
+      </div>
+    );
+  }
+
+  let totalPrice = data.reduce((total, food) => total + food.price);
+
   return (
     <div>
-      <div>
-        <table class="table">
+      <div className="container m-auto mt-5 table-responsive table-responsive-sm table-responsive-md">
+        <table class="table able-hover">
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Name</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Option</th>
+              <th scope="col">Amount</th>
             </tr>
           </thead>
+          <tbody>
+            {data.map((food, index) => {
+              <tr>
+                <th scope="now">{index + 1}</th>
+                <td>{food.name}</td>
+                <td>{food.qty}</td>
+                <td>{food.size}</td>
+                <td>{food.price}</td>
+                <td>
+                  {" "}
+                  <button type="button" className="btn p-0">
+                    <img
+                      src={trash}
+                      alt="delete"
+                      onClick={() => {
+                        dispatch({ type: "REMOVE", index: index });
+                      }}
+                    ></img>
+                  </button>
+                </td>
+              </tr>;
+            })}
+          </tbody>
         </table>
+        <div>
+          <button className="btn bg-success mt-5">Check Out</button>
+        </div>
       </div>
     </div>
   );
